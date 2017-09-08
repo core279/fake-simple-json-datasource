dd3f02489243fd96d0e7423605427ecb024455ee
core279 <core279@bliex.com>
2017-09-08 14:39:24 +0900

    Update index.js
    delta 값 변경, '/query' 에서 pie차트에 필요한 데이터 가공, port 3333 > 3335로 변경, series.json 변경

--- index.js
+++ index.js
@@ -8,14 +8,25 @@ app.use(bodyParser.json());
 var timeserie = require('./series');
 
 var now = Date.now();
+var isFirst = true;
+var delta = 0;
+for (var i = 0; i < timeserie.length; i++) {
+    var series = timeserie[i];
 
-for (var i = timeserie.length -1; i >= 0; i--) {
-  var series = timeserie[i];
-  var decreaser = 0;
-  for (var y = series.datapoints.length -1; y >= 0; y--) {
-    series.datapoints[y][1] = Math.round((now - decreaser) /1000) * 1000;
-    decreaser += 50000;
-  }
+    //1. Successes의 첫번째 아이템을 기준으로 delta 값 구하기
+    if(isFirst && series.target == "Successes"){
+        delta = Math.round(((now + 3600) - series.datapoints[0][1] ) /1000) * 1000;
+        console.info("series.target : " + series.target +  ", delta : " + delta);
+        isFirst = false;
+    }
+
+    for (var y = 0; y < series.datapoints.length; y++) {
+        // series.datapoints[y][1] = Math.round((now - decreaser) /1000) * 1000;
+        // decreaser += 50000;
+
+        series.datapoints[y][1] = delta + series.datapoints[y][1];
+
+    }
 }
 
 var annotation = {
@@ -93,29 +104,79 @@ app.all('/annotations', function(req, res) {
 })
 
 app.all('/query', function(req, res){
-  setCORSHeaders(res);
-  console.log(req.url);
-  console.log(req.body);
+    setCORSHeaders(res);
+    // console.log(req.url);
+    // console.log(req.body);
+    // console.info(req.body['targets'][0]['target']);
 
-  var tsResult = [];
-  _.each(req.body.targets, function(target) {
+    var tsResult = [];
+
+
+
+    _.each(req.body.targets, function(target) {
+
     if (target.type === 'table') {
       tsResult.push(table);
     } else {
-      var k = _.filter(timeserie, function(t) {
-        return t.target === target.target;
-      });
 
-      _.each(k, function(kk) {
+        //timeserie
+        if(req.body['targets'][0]['target'] == "Successes - Count"){
+            for (var i = timeserie.length -1; i >= 0; i--) {
+                var series = timeserie[i];
+                if(timeserie[i].target == req.body['targets'][0]['target']){
+                    console.info("series.target(success-count).points : ", series);
+                }
+            }
+        }
+
+        //timeserie 중 조회에 필요한 series만 담는다.
+        var k = _.filter(timeserie, function(t) {
+        return t.target === target.target
+        });
+
+        _.each(k, function(kk) {
         tsResult.push(kk)
-      });
+        });
     }
   });
 
-  res.json(tsResult);
-  res.end();
+
+    if(req.body['targets'][0]['target'] == "Successes - Count" || req.body['targets'][0]['target'] == "Failures - Count" ||
+        req.body['targets'][0]['target'] == "Under 1s" || req.body['targets'][0]['target'] == "1s ~ 3s" ||
+        req.body['targets'][0]['target'] == "3s ~ 5s" || req.body['targets'][0]['target'] == "Over 5s" ||
+        req.body['targets'][0]['target'] == "Errors"
+    ){
+
+        console.info(req.body['targets'][0]['target']);
+        var searchFromTimeStamp = new Date(req.body['range']['from']).getTime();
+        var searchToTimeStamp = new Date(req.body['range']['to']).getTime();
+
+        console.info("============searchFromTimeStamp : ", searchFromTimeStamp);
+        console.info("============searchToTimeStamp : ", searchToTimeStamp);
+
+        var fillterList = []; //from ~ to 사이의 timestamp만 담는다
+        var success_count_datapoints = tsResult[0]['datapoints'];
+
+        console.info("========================================================================");
+        for (var z =0; z < success_count_datapoints.length; z++){
+            if (searchFromTimeStamp <= success_count_datapoints[z][1] ){
+              if(searchToTimeStamp >= success_count_datapoints[z][1]){
+                  fillterList.push(success_count_datapoints[z]);
+              }
+            }
+        }
+        console.info("fillterList : ", fillterList);
+        console.info("========================================================================");
+        var result = JSON.parse(JSON.stringify(tsResult));
+        result[0]['datapoints'] = fillterList;
+        res.json(result);
+        res.end();
+    }else{
+        res.json(tsResult);
+        res.end();
+    }
 });
 
-app.listen(3333);
+app.listen(3335);
 
 console.log("Server is listening to port 3333");
